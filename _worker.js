f09d5f88d96186f1d82fbd57622b998654fa7080
LB999-G <LB999999LB+git@gmail.com>
2025-07-19 12:38:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = '';
+let proxyIP = 'bpb.yousef.isegaro.com';
 let DNS64Server = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
